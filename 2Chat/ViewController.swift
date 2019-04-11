affd2171f4c814026912a0780fd25e7db21144aa
William Ching <willykamus@gmail.com>
2019-04-11 16:12:40 -0400

    First Commit

---
+++ 2Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2Chat
+//
+//  Created by William Ching on 2019-04-11.
+//  Copyright © 2019 William Ching. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
